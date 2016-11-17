00a1f422c3cace40b383ba1fffb6df17157d7f92
zhiyuhuo <huozhiyunewneu@gmail.com>
2016-11-17 16:22:27 -0600

    save new furniture detector

--- furniture_classification_ml_matlab/Save_An_ML_Model.m
+++ furniture_classification_ml_matlab/Save_An_ML_Model.m
@@ -7,12 +7,13 @@ dataset = load('apartment_normal_data_1d.mat');
 dataset = dataset.data;
 rbfsigma = 20;
 kernelstr = 'Linear';
+names = {'table', 'chair', 'couch', 'bed'};
 
 category = 1;
 [model, acc] = train_a_single_furniture_svm_model(dataset, category, kernelstr);
 disp(acc);
-save(['models/model_' kernelstr '_' num2str(category) '.mat'], 'model');
-folderdir = ['models_for_ros/' num2str(category)];
+save(['models/model_' kernelstr '_' names{category} '.mat'], 'model');
+folderdir = ['models_for_ros/' names{category}];
 mkdir(folderdir);
 save_model_to_dir(folderdir, model, rbfsigma);
 
@@ -20,7 +21,7 @@ category = 2;
 [model, acc] = train_a_single_furniture_svm_model(dataset, category, kernelstr);
 disp(acc);
 save(['models/model_' kernelstr '_' num2str(category) '.mat'], 'model');
-folderdir = ['models_for_ros/' num2str(category)];
+folderdir = ['models_for_ros/' names{category}];
 mkdir(folderdir);
 save_model_to_dir(folderdir, model, rbfsigma);
 
@@ -28,7 +29,7 @@ category = 3;
 [model, acc] = train_a_single_furniture_svm_model(dataset, category, kernelstr);
 disp(acc);
 save(['models/model_' kernelstr '_' num2str(category) '.mat'], 'model');
-folderdir = ['models_for_ros/' num2str(category)];
+folderdir = ['models_for_ros/' names{category}];
 mkdir(folderdir);
 save_model_to_dir(folderdir, model, rbfsigma);
 
@@ -36,6 +37,6 @@ category = 4;
 [model, acc] = train_a_single_furniture_svm_model(dataset, category, kernelstr);
 disp(acc);
 save(['models/model_' kernelstr '_' num2str(category) '.mat'], 'model');
-folderdir = ['models_for_ros/' num2str(category)];
+folderdir = ['models_for_ros/' names{category}];
 mkdir(folderdir);
 save_model_to_dir(folderdir, model, rbfsigma);
--- furniture_classification_ml_matlab/save_model_to_dir.m
+++ furniture_classification_ml_matlab/save_model_to_dir.m
@@ -6,10 +6,13 @@ bias = model.Bias;
 group = model.SupportVectorLabels;
 scalefactor = model.Sigma;
 shift = model.Mu;
+beta = model.Beta;
 sigma = rbfsigma;
+scale = model.KernelParameters.Scale;
 
 supportvectors(find(abs(supportvectors) < 0.000001)) = 0;
 alpha(find(abs(alpha) < 0.000001)) = 0;
+beta(find(abs(beta) < 0.000001)) = 0;
 bias(find(abs(bias) < 0.000001)) = 0;
 group(find(abs(group) < 0.000001)) = 0;
 scalefactor(find(abs(scalefactor) < 0.000001)) = 0;
@@ -22,5 +25,7 @@ dlmwrite([folderdir '/group.txt'], group);
 dlmwrite([folderdir '/scalefactor.txt'], scalefactor);
 dlmwrite([folderdir '/shift.txt'], shift);
 dlmwrite([folderdir '/sigma.txt'], sigma);
+dlmwrite([folderdir '/beta.txt'], beta);
+dlmwrite([folderdir '/lscale.txt'], scale);
 
 end
